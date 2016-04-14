129e2e943c4de35940f1283d0c63853324b3f3ca
TombarthiteL <x344527085@outlook.com>
2016-04-14 23:17:30 +0800

    2

---
+++ L.NENU.Component/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace L.NENU.Component
+{
+    public class Class1
+    {
+
+
+    }
+}
--- L.NENU.Web/Controllers/TheCoreController.cs
+++ L.NENU.Web/Controllers/TheCoreController.cs
@@ -5,6 +5,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
 using L.NENU.Web.Models;
+using System.IO;
+using System.Xml;
+using L.NENU.Domain;
+using System.Text;
 
 namespace L.NENU.Web.Controllers
 {
@@ -70,8 +74,89 @@ namespace L.NENU.Web.Controllers
         {
             return View();
         }
+
+
+        /// <summary>
+        /// 接收微信发送的XML消息并且解析
+        /// </summary>
+        [HttpPost]
+        private void Valid()
+        {
+            string xmlFromWeChat = new StreamReader(Request.InputStream).ReadToEnd();
+
+            if (!string.IsNullOrEmpty(xmlFromWeChat))
+            {
+                //封装请求类
+                XmlDocument requestDocXml = new XmlDocument();
+                requestDocXml.LoadXml(xmlFromWeChat);
+                XmlElement rootElement = requestDocXml.DocumentElement;
+                WxXmlModel WxXmlModel = new WxXmlModel();
+                WxXmlModel.ToUserName = rootElement.SelectSingleNode("ToUserName").InnerText;
+                WxXmlModel.FromUserName = rootElement.SelectSingleNode("FromUserName").InnerText;
+                WxXmlModel.CreateTime = rootElement.SelectSingleNode("CreateTime").InnerText;
+                WxXmlModel.MsgType = rootElement.SelectSingleNode("MsgType").InnerText;
+                switch (WxXmlModel.MsgType)
+                {
+                    case "text"://文本
+                        WxXmlModel.Content = rootElement.SelectSingleNode("Content").InnerText;
+                        break;
+                    case "image"://图片
+                        WxXmlModel.PicUrl = rootElement.SelectSingleNode("PicUrl").InnerText;
+                        break;
+                    case "event"://事件
+                        WxXmlModel.Event = rootElement.SelectSingleNode("Event").InnerText;
+                        if (WxXmlModel.Event == "subscribe")//关注类型
+                        {
+                            WxXmlModel.EventKey = rootElement.SelectSingleNode("EventKey").InnerText;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                string resxml = getText(WxXmlModel);
+                //return WxXmlModel;
+                Response.Write(resxml);
+            }
+        }
+
+
+        private string textCase(WxXmlModel xmlMsg)
+        {
+            int nowtime = ConvertDateTimeInt(DateTime.Now);
+            string msg = "";
+            msg = getText(xmlMsg);
+            string resxml = "<xml><ToUserName><![CDATA[" + xmlMsg.FromUserName + "]]></ToUserName><FromUserName><![CDATA[" + xmlMsg.ToUserName + "]]></FromUserName><CreateTime>" + nowtime + "</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[" + msg + "]]></Content><FuncFlag>0</FuncFlag></xml>";
+            return resxml;
+        }
+
+        /// <summary>
+        /// datetime转换为unixtime
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        private int ConvertDateTimeInt(System.DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+        private int converDateTimeInt(System.DateTime time)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1));
+            return (int)(time - startTime).TotalSeconds;
+        }
+
+        private string getText(WxXmlModel xmlMsg)
+        {
+            string con = xmlMsg.Content.Trim();
+
+            System.Text.StringBuilder retsb = new StringBuilder(200);
+            retsb.Append("这里放你的业务逻辑");
+            retsb.Append("接收到的消息：" + xmlMsg.Content);
+            retsb.Append("用户的OPEANID：" + xmlMsg.FromUserName);
+
+            return retsb.ToString();
+        }
+
+
     }
 }
-
-
-
